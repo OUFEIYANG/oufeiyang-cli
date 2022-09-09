35ba4aac11167f1fc92bae5fe372bcf1b3ecebfe
ou1689166925 <oufy>
2022-09-09 19:03:38 +0800

    从头开始，现在到创建新目录了

--- bin/cli.js
+++ bin/cli.js
@@ -1,22 +1,48 @@
 #! /usr/bin/env node
 
 const program = require('commander')
+const inquirer = require('inquirer')
+const copy=require("../lib/copy")
+
 
 program
   // 定义命令和参数
-  .command('create <app-name>')
+  .command('init')
   .description('create a new project')
   // -f or --force 为强制创建，如果创建的目录存在则直接覆盖
-  .option('-f, --force', 'overwrite target directory if it exist')
+  // .option('-f, --force', 'overwrite target directory if it exist')
   .action((name, options) => {
     // 打印执行结果
-    require('../lib/create.js')(name, options)
+    inquirer.prompt([
+      {
+        type: 'input', 
+        name: 'projectNname',
+        message: 'Your project name',
+        default: 'my-app'
+      },
+      {
+        type: "list", 
+        message: "Select a framework:", 
+        name: "projectFramework", 
+        choices: ["vue", "react"],
+      },
+      {
+        type: "list", 
+        message: "Select a variant:", 
+        name: "projectVariant", 
+        choices: ["JavaScript", "TypeScript"],
+      }
+    ]).then(answers => {
+      // 打印互用输入结果
+      console.log(answers)
+      copy(answers)
+    })
   })
   
-program
-   // 配置版本号信息
-  .version(`v${require('../package.json').version}`)
-  .usage('<command> [option]')
+// program
+//    // 配置版本号信息
+//   .version(`v${require('../package.json').version}`)
+//   .usage('<command> [option]')
   
-// 解析用户执行命令传入参数
+// // 解析用户执行命令传入参数
 program.parse(process.argv);
